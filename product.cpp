66d344e80543b36c9a86f2b439600267d06b6780
charanbabu123 <82322974+charanbabu123@users.noreply.github.com>
2022-02-14 11:43:50 +0530

    Add files via upload

---
+++ product.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+ 
+using namespace std;
+ 
+int main() {
+    float a, b, product;
+    cout << "Enter two Numbers\n";
+    cin >> a >> b;
+    product = a*b;
+    cout << "Product = " << product;
+    return 0;
+}
+
+//
+OUTPUT
+enter two numbers:3.4 5.5
+product=18.7
