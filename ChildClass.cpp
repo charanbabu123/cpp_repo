40015cd665c7c3846351fcd5275bebf2784837ca
charanbabu123 <82322974+charanbabu123@users.noreply.github.com>
2022-02-23 20:24:16 +0530

    Update ChildClass.cpp

--- ChildClass.cpp
+++ ChildClass.cpp
@@ -1,14 +1,1 @@
-#include <iostream>
-using namespace std;
-class ParentClass {
-  //data member
-  public:
-    int var1 =100;
-};
-class ChildClass: public ParentClass {
-  public:
-  int var2 = 500;
-};
-int main(void) {
-  ChildClass obj;
-}
+
